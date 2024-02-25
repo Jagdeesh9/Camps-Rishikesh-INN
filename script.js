ddb2126db2a9f0765c5e4664867f37392e30cc9b
Jagdeesh Bhandari <jagdeeshbhandari05@gmail.com>
2024-02-25 19:18:25 +0530

    hello world!

---
+++ script.js
@@ -0,0 +1,13 @@
+//start navi gation bar -------------------------------------------------------------------------------------------------
+    const sidebar = document.querySelector(".sidebar");
+    const cross = document.querySelector('#cross');
+    const faBars = document.querySelector(".fa-bars")
+
+    cross.addEventListener("click",()=>{
+        sidebar.style.left = "-100%";
+    })
+
+    faBars.addEventListener('click',()=>{
+        sidebar.style.left = "0";
+    })
+//end navi gation bar -------------------------------------------------------------------------------------------------
